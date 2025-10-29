cb72c5184c6536117cc5a4f7f83f3284fc7ea421
Madhan R <madhanravi@gmail.com>
2025-10-29 21:33:20 +0530

    EP-3 Code

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "header" }, [
+// React.createElement -> React Element - JS Object -> HTML Element (Render)
+
+const heading = React.createElement(
+  "div",
+  { id: "header" },
   React.createElement(
     "h1",
     { id: "heading", className: "red" },
-    "Hello World from My React App"
-  ),
-  React.createElement(
-    "h2",
-    { id: "subheading", className: "black" },
-    "Sub Text"
-  ),
-]);
+    "Hello World from My React App 🚀"
+  )
+);
+
+// JSX -> transpiled to JS code before to reached JS engine - BABEL
+// JSX -> React.createElement -> React Element - JS Object -> HTML Element (Render)
+
+const jsxHeading = (
+  <h1 className="red">Hello World from My React App JSX 🚀</h1>
+);
+
+// Functional Component
+const Title = () => <h1>React App 🔥</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h2 className="red">Hello World from My React App Fn Component 🚀</h2>
+    <p>{100 + 200}</p>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
