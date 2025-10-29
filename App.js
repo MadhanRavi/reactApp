5f1f575c3fba111efcd36c1397a7a63cb2d5131f
Madhan R <spartanmadhan@gmail.com>
2025-10-29 19:19:21 +0530

    EP-2 Code

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "header" }, [
   React.createElement(
     "h1",
-    { id: "heading", class: "red" },
-    "Hello World from React"
+    { id: "heading", className: "red" },
+    "Hello World from My React App"
   ),
-  React.createElement("h2", { id: "subheading", class: "black" }, "Sub Text"),
+  React.createElement(
+    "h2",
+    { id: "subheading", className: "black" },
+    "Sub Text"
+  ),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # reactApp
+
+# Parcel
+
+- Dev Build
+- Local server
+- HMR - Hot Module Replacement
+- File watching Algorithm - written by C++
+- Image Optimzation
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - supports older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree shaking - remove unused code
